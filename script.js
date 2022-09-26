90cf81e07ea3746a7fc4a57f5e7dc665ecca7957
karthikeyanrock <karthisaloki123@gmail.com>
2022-09-26 17:33:10 +0530

    day 3

---
+++ script.js
@@ -0,0 +1,34 @@
+//forloop
+const n = 5;
+
+
+for (let i = 1; i <= n; i++) {
+   
+
+    console.log(i);     
+}
+
+//for in 
+
+const student = {
+    name: 'Monica',
+    class: 7,
+    age: 12
+}
+
+console.log (student.name+" "+student.class+" "+student.age );
+
+Create your own resume data in JSON format
+
+let myResume={
+    "basics": {
+      "name": "KARTHIKEYAN S",
+      "email": "KARTHI@gamil.com",
+      "phone": 96554654854,
+      "degree": "B.TECH",
+      "location": {
+        "address": "NO 90 KABHHV NDHDF H ",
+        "postalCode": 600070,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
